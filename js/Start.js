0c2f8f9a50577ee7c6add1df89409f70e0afda7b
Erhai_lake <fuzixuan0714_0826@163.com>
2023-09-17 14:34:48 +0800

    还是一言部分的更新 虽然知道没必要,但还是写了一言爱心的实时更新

--- js/Start.js
+++ js/Start.js
@@ -27,10 +27,10 @@ function Adaption() {
     }
 }
 
-// 更新一言
-Sentencehs();
 let Sentence = document.querySelector(".Sentence");
 let number = document.getElementById("heart-number");
+let intervalId = null;
+// 更新一言
 function Sentencehs() {
     let SentenceTitle = document.getElementById("SentenceTitle");
     let SentenceSource = document.getElementById("SentenceSource");
@@ -44,42 +44,49 @@ function Sentencehs() {
             from_who = data.from_who;
             from = data.from;
             if (from_who == null) {
-                from_who = "无"
+                from_who = "无";
             }
             if (from == null) {
-                from = "无"
+                from = "无";
             }
             SentenceSource.textContent = `${from_who}[${from}]`;
-            let xhr = new XMLHttpRequest();
-            let url = `https://hitokoto.cn/?uuid=${data.uuid}`;
-            xhr.open("GET", url, true);
-            xhr.send();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    let html = xhr.responseText;
-                    let div = document.createElement("div");
-                    div.innerHTML = html;
-                    let likeNumber1 = div.querySelector("#like_number1");
-                    let dataBadge = likeNumber1.getAttribute("data-badge");
-                    number.innerText = dataBadge;
-                }
-            };
+            if (intervalId != null) {
+                clearInterval(intervalId);
+            }
+            intervalId = setInterval(function () {
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", `https://hitokoto.cn/?uuid=${data.uuid}`, true);
+                xhr.send();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        const html = xhr.responseText;
+                        const div = document.createElement("div");
+                        div.innerHTML = html;
+                        const likeNumber1 = div.querySelector("#like_number1");
+                        const dataBadge = likeNumber1.getAttribute("data-badge");
+                        number.innerText = dataBadge;
+                    }
+                };
+            }, 1000);
         })
         .catch(error => {
             Sentence.title = "非常抱歉,有问题请及时反馈,谢谢";
             SentenceTitle.textContent = "发生错误";
-            SentenceSource.textContent = error
+            SentenceSource.textContent = error;
             number.innerText = "no";
         });
 }
 
-// 监听鼠标左键事件
+// 页面加载完成后更新一言
+Sentencehs();
+
+// 监听一言模块的点击事件
 Sentence.addEventListener("click", function (event) {
     Sentencehs();
-})
+});
 
-// 监听鼠标左键事件
+// 监听点赞按钮的点击事件
 number.addEventListener("click", function (event) {
-    event.stopPropagation()
+    event.stopPropagation(); // 阻止事件冒泡
     window.open("https://hitokoto.cn/?uuid=" + Sentence.title);
-})
+});
